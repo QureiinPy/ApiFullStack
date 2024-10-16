c2fd1d7ca5f1f936e2ca63bd2700410de61e4212
Dmitry <dimapolezaev6283@gmail.com>
2024-10-16 23:51:38 +0500

    Start Init

---
+++ DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskApiStack.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().ToTable("Tasks");
+        }
+
+
+    }
+}
